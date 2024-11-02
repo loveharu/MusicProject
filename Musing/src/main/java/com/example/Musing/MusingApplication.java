c2c7fb71df6f976e69414faac603faf964247134
ASUS\gkrua <gkrua5056@naver.com>
2024-11-02 21:47:56 +0900

    초기 프로젝트

---
+++ Musing/src/main/java/com/example/Musing/MusingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Musing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusingApplication.class, args);
+	}
+
+}
---
+++ Musing/src/test/java/com/example/Musing/MusingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Musing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
